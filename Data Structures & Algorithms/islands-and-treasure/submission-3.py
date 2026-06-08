ff3a1abbda10c6af193d32107328f8a0c4a37a60
Vyom Vishal Virani <vyomvirani@gmail.com>
2026-06-08 18:04:02 -0400

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+
+        def addRoom(r, c):
+            if (r<0 or c<0 or r>=rows or c>=cols or grid[r][c] == -1 or (r, c) in visit):
+                return
+            visit.add((r, c))
+            q.append((r, c))
+            grid[r][c] = distance +1
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    visit.add((r, c))
+                    q.append((r, c))
+        
+        distance = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                addRoom(r+1, c)
+                addRoom(r-1, c)
+                addRoom(r, c+1)
+                addRoom(r, c-1)
+            distance+=1
