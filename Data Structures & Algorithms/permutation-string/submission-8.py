ce788b30d28878c0e0f587766fe982e906f78c90
Vyom Vishal Virani <vyomvirani@gmail.com>
2026-06-09 15:40:29 -0400

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l, r = 0, len(s1)
+
+        while r<=len(s2):
+            if sorted(s1) == sorted(s2[l:r]):
+                return True
+            else:
+                l+=1
+                r+=1
+        return False
