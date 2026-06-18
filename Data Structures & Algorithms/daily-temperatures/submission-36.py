2464387ac6598fe8aa6c1de2e70979a74e91f66e
Vyom Vishal Virani <vyomvirani@gmail.com>
2026-06-18 08:54:19 -0400

    Add: daily-temperatures - submission-36

---
+++ Data Structures & Algorithms/daily-temperatures/submission-36.py
@@ -0,0 +1,9 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            for j in range(i+1, len(temperatures)):
+                if temperatures[j]>temperatures[i]:
+                    res[i] = j-i
+                    break
+        return res
